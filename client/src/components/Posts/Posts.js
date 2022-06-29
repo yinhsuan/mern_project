d2fb05a962fdc5c18f5ac3066a98e461e5507f65
yinhsuan <sandy10235168@gmail.com>
2022-06-29 23:21:00 +0800

    add redux

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Post from './Post/Post';
+import { useSelector } from 'react-redux';
+
 import useStyles from './styles';
+import Post from './Post/Post';
 
 
 const Posts = () => {
     const classes = useStyles();
+    const posts = useSelector((state) => state.posts);
+    console.log('post:',posts);
 
     return (
         <>
