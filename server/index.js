e8ee676a63f9649259cf7d87a78d61af251fe2d9
yinhsuan <sandy10235168@gmail.com>
2022-06-30 15:04:50 +0800

    add dotenv to use .env

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,10 +15,10 @@ app.use(cors());
 app.use('/posts', postRoutes); // http://localhost:7777 => http://localhost:7777/posts
 
 // https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = "mongodb+srv://sandy:0123@cluster0.s6fz0.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL = "mongodb+srv://sandy:0123@cluster0.s6fz0.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 7777;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error));
 
